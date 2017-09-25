21d889681374a316010274ca21f03004ec3af6b5
Atmar Kohistany <atma0004@edu.eal.dk>
2017-09-25 08:51:32 +0200

    Add project files.

---
+++ switch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByValue
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+            valuetype(value);
+            Console.WriteLine(value);
+            PassByRef(ref value);
+            Console.WriteLine(value);
+            valuetype(value);
+            Console.WriteLine(value);
+
+        }
+        static void valuetype(int myvalue)
+        {
+            myvalue += 5;
+            Console.WriteLine("my value={0}",myvalue);
+        }
+        static void PassByRef(ref int myrefvalue)
+        {
+            myrefvalue += 5;
+            Console.WriteLine("Myrefvalue {0}",myrefvalue);
+        }
+    }
+}
